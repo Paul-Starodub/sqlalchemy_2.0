6078181d2de7a4290f9ef0d0195a8623368efc29
Pavlo Starobub <starodub.pavlo@alsodev.md>
2025-12-16 15:39:46 +0200

    Initial

---
+++ basics.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session
+
+
+engine = create_engine("sqlite:///mydatabase.db", echo=True)
+
+
+conn = engine.connect()
+
+conn.execute(text("CREATE TABLE IF NOT EXISTS people (name str, age int)"))
+conn.commit()
+
+
+session = Session(engine)
+
+session.execute(text("INSERT INTO people (name, age) VALUES ('John', 30)"))
+session.commit()
